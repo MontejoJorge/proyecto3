158ec23e977226e2873645e345946f55c3624dfb
AlejandroPerezAnuncibay <alejandro.perez@ikasle.egibide.org>
2021-01-18 17:28:57 +0100

    Cambio de posicion mapa graficos

--- js/mapa.js
+++ js/mapa.js
@@ -1,13 +1,13 @@
 var map = L.map('mapid').setView([42.846841, -2.670996], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
 
 L.marker([42.867209, -2.677539]).addTo(map)
-    //.bindPopup('Este es mi instituto.<br> Arriaga.')
-//.openPopup()
+.bindPopup('Este es mi instituto.<br> Arriaga.')
+.openPopup()
 ;
 
-L.marker([42.847986, -2.677383]).addTo(map);
+L.marker([42.847986, -2.677383]).addTo(map)
+    .bindPopup('Este no es mi instituto.<br> Arriaga.')
+    .openPopup();
 L.marker([42.852048, -2.672202]).addTo(map);
