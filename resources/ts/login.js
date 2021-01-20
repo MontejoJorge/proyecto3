5179280da175c7c47a06da72c479c7b7aefe2a41
unaivadillo <unai.vadillo@ikasle.egibide.org>
2021-01-20 10:55:04 +0100

    validaciones acabadas en el registro

--- resources/ts/login.js
+++ resources/ts/login.js
@@ -1,7 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var $ = require("jquery");
 $(document).ready(function () {
+    $("#registrar").click(function () {
+        validarForm();
+    });
 });
 // Validaciones
 function validarNombre(nombre) {
@@ -63,6 +63,33 @@ function validarContraseña(contraseña, contraseña2) {
         throw "La contraseña debe contener 8 o más caracteres, una mayuscula, una minuscula, un numero y un caracter especial (@#$%&?¿!¡._-)";
     }
 }
+function validarPortal(texto) {
+    var regex = new RegExp("^(([0-9]){1,2}[a-zA-Z]?)$");
+    if (!regex.test(texto)) {
+        throw "Introduce el portal correctamente";
+    }
+    else {
+        return true;
+    }
+}
+function validarPiso(texto) {
+    var regex = new RegExp("^([0-9]){1,3}$");
+    if (!regex.test(texto)) {
+        throw "Introduce el piso correctamente";
+    }
+    else {
+        return true;
+    }
+}
+function validarTlf(tlf) {
+    var regex = new RegExp("^((6|7|8|9)[-]*([0-9][-]*){8})$");
+    if (!regex.test(String(tlf))) {
+        throw "Introduce el tlf correctamente Ej:688234567";
+    }
+    else {
+        return true;
+    }
+}
 function validarNoBlanco(texto) {
     if (/^\s+|\s+$/.test(texto)) {
         return true;
@@ -74,19 +101,34 @@ function validarNoBlanco(texto) {
 //Registrarse
 function validarForm() {
     try {
+        //Recogemos los datos introducidos por el usuario
         var nombre = $("#nombre").val().toString();
         var apellido = $("#apellido").val().toString();
         var dni = $("#dni").val().toString();
         var email = $("#email").val().toString();
+        var password1 = $("#password").val().toString();
+        var password2 = $("#password2").val().toString();
+        var portal = $("#portal").val().toString();
+        var piso = $("#piso").val().toString();
+        var direccion = $("#form-address").val().toString();
+        var fecha_nac = $("#fecha_nac").val().toString();
+        // @ts-ignore
+        var provincia = $('select[name ="provincia"]').val();
+        // @ts-ignore
+        var mano = $('select[name ="mano"]').val();
+        // @ts-ignore
+        var tlf = $("#ntel").val();
         //llamamos a los metodos que validan los campos
         validarDNI(dni);
-        //Comprobamos que no este vacio el nombre ni el apellido
+        validarEmail(email);
+        //Comprobamos que no este vacio el nombre ni el apellido y que no tienen ambos espacios en blanco
         if (nombre == "" && apellido == "") {
             throw "El nombre y apellido no pueden estar vacios";
         }
         else if (validarNoBlanco(nombre) && validarNoBlanco(apellido)) {
             throw "El nombre y apellido no pueden contener espacios";
         }
+        //Validamos el nombre comprobando antes que no tiene espacios en blanco
         if (nombre == "") {
             throw "El nombre no puede estar vacio";
         }
@@ -96,6 +138,7 @@ function validarForm() {
         else {
             validarNombre(nombre);
         }
+        //Validamos el apellido comprobando antes que no tiene espacios en blanco
         if (apellido == "") {
             throw "El apellido no puede estar vacio";
         }
@@ -105,8 +148,54 @@ function validarForm() {
         else {
             validarApellido(apellido);
         }
-        validarEmail(email);
-        validarContraseña($("#password").eq(0).val(), $("#password2").val());
+        //Validar para que no nos pongan la contraseña en blanco o con espacios
+        if (password1 == "" && password2 == "") {
+            throw "La contraseña no puede estar vacia";
+        }
+        else if (validarNoBlanco(password1) && validarNoBlanco(password2)) {
+            throw "La contraseña no puede tener espacios en blanco";
+        }
+        else {
+            validarContraseña($("#password").eq(0).val(), $("#password2").val());
+        }
+        //Validamos para que tengan que seleccionar una obligatoriamente
+        if (provincia == null) {
+            throw "Seleccione su provincia de nacimiento";
+        }
+        //Validamos la fecha para que no la dejen vacia
+        if (fecha_nac == "") {
+            throw "Seleccione su fecha de nacimiento";
+        }
+        //Validamos la direccion para que no este vacio
+        if (direccion == "") {
+            throw "Rellene el campo direccion";
+        }
+        //Validamos el portal para que no este vacio ni mal introducido
+        if (portal == "") {
+            throw "El portal no puede estar vacio";
+        }
+        else if (validarNoBlanco(portal)) {
+            throw "El portal no puede tener espacios en blanco";
+        }
+        else {
+            validarPortal(portal);
+        }
+        //Validamos el piso para que no este vacio ni mal introducido
+        if (piso == "") {
+            throw "El piso no puede estar vacio";
+        }
+        else if (validarNoBlanco(piso)) {
+            throw "El piso no puede tener espacios en blanco";
+        }
+        else {
+            validarPiso(piso);
+        }
+        //Validamos para que tengan que seleccionar una obligatoriamente
+        if (mano == null) {
+            throw "Seleccione la mano de su piso";
+        }
+        //Validamos el numero de telefono
+        validarTlf(tlf);
         alert("Usuario registrado correctamente");
         window.location.href = "login.html";
         return true;
--- resources/ts/login.ts
+++ resources/ts/login.ts
@@ -1,7 +1,8 @@
-import * as $ from "jquery";
 
 $(document).ready(function () {
-
+$("#registrar").click(function (){
+    validarForm();
+})
 });
 // Validaciones
 function validarNombre(nombre:string):boolean{
@@ -56,6 +57,30 @@ function validarContraseña(contraseña, contraseña2):boolean{
         throw "La contraseña debe contener 8 o más caracteres, una mayuscula, una minuscula, un numero y un caracter especial (@#$%&?¿!¡._-)";
     }
 }
+function validarPortal(texto:string){
+    var regex = new RegExp("^(([0-9]){1,2}[a-zA-Z]?)$");
+    if (!regex.test(texto)){
+        throw "Introduce el portal correctamente";
+    }else{
+        return true;
+    }
+}
+function validarPiso(texto:string){
+    var regex = new RegExp("^([0-9]){1,3}$");
+    if (!regex.test(texto)){
+        throw "Introduce el piso correctamente";
+    }else{
+        return true;
+    }
+}
+function validarTlf(tlf:number){
+    var regex = new RegExp("^((6|7|8|9)[-]*([0-9][-]*){8})$");
+    if (!regex.test(String(tlf))){
+        throw "Introduce el tlf correctamente Ej:688234567";
+    }else{
+        return true;
+    }
+}
 function validarNoBlanco(texto:string):boolean {
     if(/^\s+|\s+$/.test(texto)) {
         return true;
@@ -66,19 +91,38 @@ function validarNoBlanco(texto:string):boolean {
 //Registrarse
 function validarForm():boolean {
     try {
+        //Recogemos los datos introducidos por el usuario
         let nombre: string = $("#nombre").val().toString();
         let apellido: string = $("#apellido").val().toString();
         let dni: string = $("#dni").val().toString();
         let email: string = $("#email").val().toString();
+        let password1: string = $("#password").val().toString();
+        let password2: string = $("#password2").val().toString();
+        let portal: string = $("#portal").val().toString();
+        let piso: string = $("#piso").val().toString();
+        let direccion: string= $("#form-address").val().toString();
+        let fecha_nac: string = $("#fecha_nac").val().toString();
+
+
+        // @ts-ignore
+        let provincia:string= $('select[name ="provincia"]').val();
+        // @ts-ignore
+        let mano:string =$('select[name ="mano"]').val();
+        // @ts-ignore
+        let tlf: number = $("#ntel").val();
 
         //llamamos a los metodos que validan los campos
         validarDNI(dni);
-        //Comprobamos que no este vacio el nombre ni el apellido
+        validarEmail(email);
+
+        //Comprobamos que no este vacio el nombre ni el apellido y que no tienen ambos espacios en blanco
         if (nombre == "" && apellido == "") {
             throw "El nombre y apellido no pueden estar vacios";
         } else if (validarNoBlanco(nombre) && validarNoBlanco(apellido)) {
             throw "El nombre y apellido no pueden contener espacios"
         }
+
+        //Validamos el nombre comprobando antes que no tiene espacios en blanco
         if (nombre == "") {
             throw "El nombre no puede estar vacio";
         } else if (validarNoBlanco(nombre)) {
@@ -87,6 +131,7 @@ function validarForm():boolean {
             validarNombre(nombre);
         }
 
+        //Validamos el apellido comprobando antes que no tiene espacios en blanco
         if (apellido == "") {
             throw "El apellido no puede estar vacio";
         }else if (validarNoBlanco(apellido)){
@@ -95,8 +140,56 @@ function validarForm():boolean {
         }else{
             validarApellido(apellido);
         }
-        validarEmail(email);
-        validarContraseña($("#password").eq(0).val(),$("#password2").val());
+
+        //Validar para que no nos pongan la contraseña en blanco o con espacios
+        if (password1 == "" && password2 =="") {
+            throw "La contraseña no puede estar vacia";
+        } else if (validarNoBlanco(password1)&& validarNoBlanco(password2)) {
+            throw "La contraseña no puede tener espacios en blanco"
+        } else {
+            validarContraseña($("#password").eq(0).val(),$("#password2").val());
+        }
+
+        //Validamos para que tengan que seleccionar una obligatoriamente
+        if (provincia==null){
+            throw "Seleccione su provincia de nacimiento"
+        }
+
+        //Validamos la fecha para que no la dejen vacia
+        if (fecha_nac==""){
+            throw "Seleccione su fecha de nacimiento"
+        }
+
+        //Validamos la direccion para que no este vacio
+        if (direccion==""){
+            throw "Rellene el campo direccion"
+        }
+        //Validamos el portal para que no este vacio ni mal introducido
+        if (portal == "") {
+            throw "El portal no puede estar vacio";
+        }else if (validarNoBlanco(portal)){
+            throw "El portal no puede tener espacios en blanco"
+        }else{
+            validarPortal(portal);
+        }
+
+        //Validamos el piso para que no este vacio ni mal introducido
+        if (piso == "") {
+            throw "El piso no puede estar vacio";
+        }else if (validarNoBlanco(piso)){
+            throw "El piso no puede tener espacios en blanco"
+        }else{
+            validarPiso(piso);
+        }
+
+        //Validamos para que tengan que seleccionar una obligatoriamente
+        if (mano==null){
+            throw "Seleccione la mano de su piso"
+        }
+
+        //Validamos el numero de telefono
+        validarTlf(tlf);
+
         alert("Usuario registrado correctamente");
         window.location.href="login.html";
         return true;
