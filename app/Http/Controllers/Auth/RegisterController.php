236039a5ce2092cea668ae8e4b09b9f991969330
Jorge Montejo <56157795+MontejoJorge@users.noreply.github.com>
2021-01-21 18:34:34 +0100

    login/register solicitante

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:solicitantes'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
     }
 
@@ -65,9 +65,20 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'phone' => $data['phone'],
+            'name' => $data['name'],
+            'surname' => $data['surname'],
+            'dni' => $data['dni'],
+            'birthdate' => $data['birthdate'],
+            'place_of_birth' => $data['place_of_birth'],
+            'postal_code' => $data['postal_code'],
+            'street_name' => $data['street_name'],
+            'number' => $data['number'],
+            'floor' => $data['floor'],
+            'door' => $data['door'],
+            'city' => $data['city'],
         ]);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,6 +10,54 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
+                        <input type="text" name="email" placeholder="email" value="{{ old("email") }}"><br>
+                        <input type="password" name="password" placeholder="password"><br>
+                        <input type="password" name="password_confirmation" placeholder="Repeat the password"><br>
+                        <input type="number" name="phone" placeholder="phone" value="{{ old("number") }}"><br>
+                        <input type="text" name="name" placeholder="name" value="{{ old("name") }}"><br>
+                        <input type="text" name="surname" placeholder="surname" value="{{ old("surname") }}"><br>
+                        <input type="text" name="dni" placeholder="dni" value="{{ old("dni") }}"><br>
+                        <input type="date" name="birthdate" placeholder="birthdate" value="{{ old("birthdate") }}"><br>
+                        <input type="text" name="place_of_birth" placeholder="place_of_birth" value="{{ old("place_of_birth") }}"><br>
+                        <input type="text" name="postal_code" placeholder="postal_code" value="{{ old("postal_code") }}"><br>
+                        <input type="text" name="street_name" placeholder="street_name" value="{{ old("street_name") }}"><br>
+                        <input type="number" name="number" placeholder="number" value="{{ old("number") }}"><br>
+                        <input type="text" name="floor" placeholder="floor" value="{{ old("floor") }}"><br>
+                        <input type="text" name="door" placeholder="door" value="{{ old("door") }}"><br>
+                        <input type="text" name="city" placeholder="city" value="{{ old("city") }}"><br>
+
+                    @if ($errors->any())
+                        @foreach ($errors->all() as $e)
+                            {{ $e }}
+                        @endforeach
+                    @endif
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+{{-- @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
 
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
@@ -74,4 +122,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
