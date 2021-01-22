f84f552902f2c2813f56842496ba781bdea7733d
unaivadillo <unai.vadillo@ikasle.egibide.org>
2021-01-22 10:57:14 +0100

    fin clase

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
 Route::get('/login', function () {
-    return view('index');
+    return view('login');
 })->name('login');
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
