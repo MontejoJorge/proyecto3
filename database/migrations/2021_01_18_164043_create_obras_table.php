f986d5f3d5a5ea005626f8358f28efed4de1f964
Jorge Montejo <56157795+MontejoJorge@users.noreply.github.com>
2021-01-23 19:37:00 +0100

    direccion obra

--- database/migrations/2021_01_18_164043_create_obras_table.php
+++ database/migrations/2021_01_18_164043_create_obras_table.php
@@ -15,10 +15,28 @@ class CreateObrasTable extends Migration
     {
         Schema::create('obras', function (Blueprint $table) {
             $table->id();
+            //Foreign Keys
             $table->foreignId("requestor_id")->constrained("users");
-            $table->foreignId("building_id")->constrained("tipos_edificios");
-            $table->foreignId("construction_id")->constrained("tipos_obras");
+            $table->foreignId("building_type")->constrained("tipos_edificios");
+            $table->foreignId("construction_type")->constrained("tipos_obras");
             $table->foreignId("worker_id")->nullable()->constrained("users");
+
+            //Direccion
+            $table->string("street_name");
+            $table->string("number");
+            $table->string("floor");
+            $table->string("door");
+            $table->integer("postal_code");
+            $table->string("city");
+            $table->string("province");
+
+            //Fechas
+            $table->date("start_date")->useCurrent()->nullable();
+            $table->date("end_date")->nullable();
+
+            //Otros
+            $table->text("description");
+            $table->longText("blueprint")->nullable();
             $table->timestamps();
         });
     }
